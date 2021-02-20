57ef82fa46a7f5a4dc857af07af402c02d1302bd
Lohit <lohitanant@yahoo.in>
2021-02-21 01:15:46 +0530

    Change are ready to commit

--- index.js
+++ index.js
@@ -4,3 +4,12 @@
 //3: Multiline code
 //4: use (_) to get the last result
 //5: we can use editor mode 
+
+const fs = require("fs");
+
+//creating a new file
+//fs.writeFileSync('read.txt','welcome to my node js.');
+
+//fs.writeFileSync('read.txt','this is trial version.');
+
+fs.appendFileSync('read.txt','thank You.');
