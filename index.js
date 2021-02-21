ba6319a40eb05c7e7755236b1a67c477eca84366
Lohit <lohitanant@yahoo.in>
2021-02-21 12:28:05 +0530

    this is node js

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-//REPL
-//1: JS expression
-//2: use variables
-//3: Multiline code
-//4: use (_) to get the last result
-//5: we can use editor mode 
+const app = require('express')();
+const PORT = 8080;
 
-const fs = require("fs");
+app.listen(
+    PORT,
+    () => console.log(`it's alive on http://localhost:${PORT}`)
+);
 
-//creating a new file
-//fs.writeFileSync('read.txt','welcome to my node js.');
-
-//fs.writeFileSync('read.txt','this is trial version.');
-
-fs.appendFileSync('read.txt','thank You.');
+app.get('/tshirt' ,(req  ,res) =>{
+    res.status(200).send({
+        tshirt : "🌹",
+        size : 'large'
+    })
+});
